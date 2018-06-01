ddb2a40b644ba895c87add6d6eb935db39c3ccad
Stefan Bojnak <xbojnak@fi.muni.cz>
2018-06-02 00:24:43 +0200

    Added scenario

---
+++ BrowserEfficiencyTest/Scenarios/IsMuniScenario.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Remote;
+
+namespace BrowserEfficiencyTest.Scenarios
+{
+    class IsMuniScenario : Scenario
+    {
+
+        public IsMuniScenario()
+        {
+            this.Name = "ismuniscenario";
+        }
+
+        public override void Run(RemoteWebDriver driver, string browser, CredentialManager credentialManager, ResponsivenessTimer timer)
+        {
+            driver.NavigateToUrl("https://www.muni.cz/");
+            driver.Wait(2);
+            
+            driver.TypeIntoField(driver.FindElementById("search"), "PV260" + Keys.Enter);
+            driver.Wait(2);
+
+            driver.FindElementsByClassName("gs-title").First().Click();
+            driver.WaitForPageLoad();
+            driver.Wait(2);
+
+        }
+    }
+}
